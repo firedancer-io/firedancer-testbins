3e8bc8e9a98fd775bb38d38bc809d6eff0d3da34
Anway De <ande@jumptrading.com>
2024-05-13 13:42:34 -0500

    ms1.4: remove runtime submodule for ci

--- 2/fd_tests.h
+++
@@ -1,1 +0,0 @@
-#include "../../../flamenco/runtime/tests/fd_tests.h"
--- Local.mk
+++
@@ -1,4 +0,0 @@
-ifdef FD_HAS_INT128
-$(call make-unit-test,test_native_programs,test_native_programs fd_tests,fd_ballet fd_funk fd_util fd_sol_tests fd_flamenco,$(SECP256K1_LIBS))
-$(call run-unit-test,test_native_programs)
-endif
